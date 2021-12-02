cc5e0d7c6ee8ab9feb9fb7623f962eb1f7c6fb7f
Lucie Halloucherie <lhalloucheri@iutbayonne.univ-pau.fr>
2021-12-02 08:28:30 +0100

    Creation du controleur metier

---
+++ src/Controller/ProstagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProstagesController extends AbstractController
+{
+    /**
+     * @Route("/prostages", name="prostages")
+     */
+    public function index(): Response
+    {
+        return $this->render('prostages/index.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+}
