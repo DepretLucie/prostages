f6e6f76eed83ed1aae01fdd28b940f22c6fc55ba
Lucie Halloucherie <lhalloucheri@iutbayonne.univ-pau.fr>
2022-01-05 11:59:00 +0100

    Creation de l'entité Formation

---
+++ src/Entity/Formation.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FormationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FormationRepository::class)
+ */
+class Formation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $nomCourt;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $nomLong;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomCourt(): ?string
+    {
+        return $this->nomCourt;
+    }
+
+    public function setNomCourt(string $nomCourt): self
+    {
+        $this->nomCourt = $nomCourt;
+
+        return $this;
+    }
+
+    public function getNomLong(): ?string
+    {
+        return $this->nomLong;
+    }
+
+    public function setNomLong(string $nomLong): self
+    {
+        $this->nomLong = $nomLong;
+
+        return $this;
+    }
+}
